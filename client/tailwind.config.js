ddd4a8bea3d571f748f1e03c0760cc0220481671
Rishabh Tiwari <13rishabhtiwari@gmail.com>
2024-04-14 14:00:01 +0530

    added landing bg in static

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "landing-bg": "url('https://cdn.dribbble.com/users/427857/screenshots/13993146/media/da6ec8e324070106c8d30a31b4f97c9a.jpg')"
+        "landing-bg": "url('./static/landing-bg.jpg')"
       }
     },
   },
