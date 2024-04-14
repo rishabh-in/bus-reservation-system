37987ee4cb09b8b921a247d7e0c5b624435332c7
Rishabh Tiwari <13rishabhtiwari@gmail.com>
2024-04-14 14:43:57 +0530

    generate token function added

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import dotevn from 'dotenv';
 import socketConfig from './config/socketConfig.js';
 import connectDB from './config/connectDB.js';
 import logger from './middleware/logs.js';
+import configRoutes from './routes/index.js';
 
 dotevn.config()
 connectDB();
@@ -16,6 +17,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(logger);
+
+configRoutes(app);
 
 server.listen(process.env.PORT, () => {
   console.log("Server started running on ", process.env.PORT);
